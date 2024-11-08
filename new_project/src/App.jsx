6ecb8a424357dc5328b80a353667d7c0697fbb8d
contact91 <chouvelquentin91@gmail.com>
2024-11-08 09:53:15 +0100

    Navbar added, Tailwind installed

--- new_project/src/App.jsx
+++ new_project/src/App.jsx
@@ -1,15 +1,23 @@
 import Starfield from "react-starfield";
 
+import Home from "./assets/pages/Home";
+
 function App() {
   return (
     <>
       <div>
-        <Starfield
-          starCount={1000}
-          starColor={[255, 255, 255]}
-          speedFactor={0.05}
-          backgroundColor="black"
-        />
+        <div className="">
+          <Starfield
+            alphaFactor={1.9}
+            starCount={10000}
+            starColor={[255, 255, 255]}
+            speedFactor={0.05}
+            backgroundColor="black"
+          />
+        </div>
+
+        <Home />
+        <h1 className="text-black bg-orange-700/100 text-[100px]">Hello</h1>
       </div>
     </>
   );
---
+++ new_project/src/assets/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <div className="z-10 bg-gradient-to-l from-orange-200 to-orange-900 p-4 w-full">
+      <h1 className="text-white ">Hello Wold, from Mars</h1>
+    </div>
+  );
+}
+
+export default Navbar;
